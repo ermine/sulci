1eb8fff7b67986c84aaf4e8f27ec3f316ab8b645
Anastasia Gornostaeva <ermine@ermine.pp.ru>
2005-01-28 10:21:03 +0000

    additional math functions for plugin_calc

---
+++ math.ml
@@ -0,0 +1,23 @@
+(*                                                                          *)
+(* (c) 2004, Anastasia Gornostaeva. <ermine@ermine.pp.ru>                   *)
+(*                                                                          *)
+
+exception MathNumberTooBig
+exception MathNegNumber
+exception MathCannotFloatFact
+
+let fact x =
+   if x > 174.0 then raise MathNumberTooBig
+   else if x -. floor x <> 0.0 then raise MathCannotFloatFact
+   else if x < 0.0 then raise MathNegNumber
+   else
+      let rec f y acc =
+	 if y = 0.0 then acc
+	 else f (y -. 1.) (y *. acc)
+      in
+	 f x 1.
+
+let fib n =
+   let phi = (sqrt(5.) +. 1.) /. 2. in
+   let ihp = 1. -. phi in
+      (phi ** n -. ihp ** n) /. sqrt(5.)
