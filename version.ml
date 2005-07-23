b1d7fb099c90a9ad563e33aedd609f95835e3247
Anastasia Gornostaeva <ermine@ermine.pp.ru>
2005-07-23 13:00:01 +0000

    updated

--- version.ml
+++ version.ml
@@ -2,4 +2,4 @@
 (* (c) 2004, 2005 Anastasia Gornostaeva. <ermine@ermine.pp.ru>              *)
 (*                                                                          *)
 
-let version="0.5.alpha 20050722"
+let version="0.5.alpha 20050723"
