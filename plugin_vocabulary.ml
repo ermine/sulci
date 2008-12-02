c0779c346d2590b7552485fe71dc0e7b76dd93c1
Anastasia Gornostaeva <ermine@ermine.pp.ru>
2008-12-02 12:45:52 +0000

    * sulci/plugin_vocabulary.ml: New command: wtfremove key<=value> with MUC role checking

--- plugin_vocabulary.ml
+++ plugin_vocabulary.ml
@@ -248,12 +248,71 @@ let wtffind text event from xml out =
     in
       output_records sql xml out
 
+let wtfremove_re = Pcre.regexp ~flags:[`DOTALL; `UTF8] "([^=]+)(\\s*=\\s*(.*))?"
+
+let wtfremove text event from xml out =
+  let key, value =
+    try
+      let res = Pcre.exec ~rex:wtfremove_re text in
+      let key = Pcre.get_substring res 1 in
+      let value = try Pcre.get_substring res 3 with Not_found -> "" in
+        key, value
+    with Not_found ->
+      ("", "") in
+    if key = "" then
+      make_msg out xml 
+        (Lang.get_msg ~xml "plugin_vocabulary_invalid_syntax" [])
+    else
+      let room = from.lnode, from.ldomain in
+      let cond =
+        if GroupchatMap.mem room !groupchats then
+          let room_env = GroupchatMap.find room !groupchats in
+          let item = Nicks.find from.lresource room_env.nicks in
+            if item.role = `Moderator then
+              ""
+            else
+              match item.jid with
+                | None ->
+                    " AND nick=" ^ escape from.lresource ^
+                      " AND luser=" ^ escape from.lnode ^
+                      " AND lserver=" ^ escape from.ldomain
+                | Some j ->
+                    " AND luser=" ^ escape j.lnode ^
+                      " AND lserver=" ^ escape j.ldomain
+        else
+          " AND luser=" ^ escape from.lnode ^
+            " AND lserver=" ^ escape from.ldomain
+      in
+      let sql cmd =
+        if value = "" then
+          Printf.sprintf
+            "%s FROM %s WHERE key=%s %s" cmd table (escape key) cond
+        else
+          Printf.sprintf
+            "%s FROM %s WHERE key=%s AND value=%s %s"
+            cmd table (escape key) (escape value) cond
+      in
+        let records =
+          match get_one_row file db (sql "SELECT count(*)") with
+            | None -> 0
+            | Some r -> Int64.to_int (int64_of_data r.(0))
+        in
+          if records > 0 then (
+            simple_exec file db (sql "DELETE");
+            make_msg out xml (Lang.get_msg ~xml "plugin_vocabulary_removed" [])
+          )
+          else
+            make_msg out xml (Lang.get_msg ~xml
+                                "plugin_vocabulary_nothing_to_remove" [])
+
+
 let _ =
   List.iter (fun (command, callback) ->
                Hooks.register_handle (Hooks.Command (command, callback)))
-    ["wtfall", wtfall;
+    ["wtf", wtf;
+     "wtfall", wtfall;
      "wtfrand", wtfrand;
      "wtfcount", wtfcount;
      "dfn", dfn;
-     "wtffind", wtffind]
-    
+     "wtffind", wtffind;
+     "wtfremove", wtfremove]
